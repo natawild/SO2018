d6d26fa313d333f71be3dc99bf1a8eb8fa8e5ba9
Célia Figueiredo <celianlfg@hotmail.com>
2018-03-07 15:35:12 +0000

    guiao3

---
+++ Guioes/Guiao3/ex1.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * argv[]) {
+
+execlp("ls","COISINHO","-l",NULL);
+perror ("Não funcionou");
+
+return 1;
+
+}
---
+++ Guioes/Guiao3/ex4.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main (int argc, char * argv[]) {
+
+	//strncpy(argv[1],"12345",)
+
+	execv("./ex3",&argv[1]);
+	perror("Não funcinou");
+
+	return 1;
+
+}				
