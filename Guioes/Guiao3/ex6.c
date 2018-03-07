83f04d73ccef63933d89c3ced5e1bb55c7f88479
Célia Figueiredo <celianlfg@hotmail.com>
2018-03-07 15:51:38 +0000

    ex6

---
+++ Guioes/Guiao3/ex6.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int mysystem(const char *command); //não estou autorizado a mexer neste comando porque é um const 
+
+int  main() {
+	mysystem(); 
+
+}
+
+//Implemntar e testar 
+int mysystem(char * cmd){
+
+
+}
