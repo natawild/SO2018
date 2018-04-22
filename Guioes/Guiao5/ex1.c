4fcdc9c265c197c1fd122da28102a039678d41c4
Célia Figueiredo <celianlfg@hotmail.com>
2018-04-22 18:13:41 +0100

    guiar 5 ex1

--- Guioes/Guiao5/ex1.c
+++ Guioes/Guiao5/ex1.c
@@ -14,15 +14,21 @@ int main(){
 	pipe(pfds); 
 
 	if(!fork()){
+		close(pfds[1]);
+		n = read(pfds[0], buffer, sizeof(buffer));
+		/*
 		printf("FILHO a ler\n");
 		n = read(pfds[0], buffer, sizeof(buffer));
 		printf("FILHO leu: %d %s\n",n ,buffer );
 		n = read(pfds[0], buffer, sizeof(buffer));
-		printf("FILHO leu: %d %s\n",n ,buffer );
+		printf("FILHO leuuuuuu: %d %s\n",n ,buffer );
+		*/
+		write(1,buffer,n); 
 		exit(0); 
 	} 
 	else{
-		sleep(5);
+		sleep(6);
+		close(pfds[0]); 
 		//getchar(); 
 		printf("Pai a escrever\n");
 		write(pfds[1], "linha de teste muito comprida a ver que disparates vão acontecer se funciona ou não\n", 200);
